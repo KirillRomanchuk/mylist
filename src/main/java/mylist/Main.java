5b392f60dc2abcdab449dbd6898a4ff59a986d17
Kirill Romanchuk <kyrylo.romanchuk@gmail.com>
2019-04-01 15:21:35 +0300

    my list start

---
+++ src/main/java/mylist/Main.java
@@ -0,0 +1,71 @@
+package mylist;
+
+import mylist.my_array_list.MyList;
+import mylist.my_array_list.MyArrayList;
+import mylist.my_hash_map.MyHashMap;
+import mylist.my_hash_map.MyMap;
+import mylist.my_linked_list.Linked;
+import mylist.my_linked_list.MyLinkedList;
+
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        CollectionTask collectionTask = new CollectionTask();
+
+        Integer[] ints = {4, 5, -6, 4, 5, 3, 4, 2, 4, 5, 7};
+
+        collectionTask.receiveNumberOfEntry(ints).forEach((k, v) -> System.out.println(k + "- " + v));
+
+        List<Integer> integers = new ArrayListWithOutRemove<>();
+        integers.add(1);
+        integers.remove(0);
+
+
+        MyList<String> myArrayList = new MyArrayList<>();
+        myArrayList.add("1");
+        myArrayList.add("2");
+        myArrayList.add("3");
+        myArrayList.add("4");
+
+        myArrayList.forEach(s -> System.out.println(s));
+
+        System.out.println("//---------------------------------");
+
+        Linked<String> strings = new MyLinkedList<>();
+        strings.addFirst("1");
+        strings.addFirst("2");
+        strings.addFirst("3");
+        strings.addLast("4");
+
+        strings.remove(2);
+
+        for (String string : strings) {
+            System.out.println(string);
+        }
+
+        System.out.println("//---------------------------------");
+
+
+        MyMap<String, String> stringsMap = new MyHashMap<>();
+        stringsMap.insert("1", "1");
+        stringsMap.insert("2", "2");
+        stringsMap.insert("3", "3");
+        stringsMap.insert("4", "4");
+        stringsMap.insert("5", "5");
+        stringsMap.insert("6", "6");
+
+        stringsMap.delete("4");
+
+
+        for (String s : stringsMap) {
+            System.out.println(s);
+        }
+
+
+
+
+    }
+}
---
+++ src/main/java/mylist/my_linked_list/DescendingIterator.java
@@ -0,0 +1,8 @@
+package mylist.my_linked_list;
+
+import java.util.Iterator;
+
+public interface DescendingIterator<E> {
+
+    Iterator<E> descendingIterator();
+}
